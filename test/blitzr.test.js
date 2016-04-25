be5b9d5d72b234f72473e710f01603feb67f83dc
yoann <yoann@blitzr.com>
2016-04-25 16:49:24 +0200

    add unit test

---
+++ test/blitzr.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const blitzr = require ('../blitzr.js');
+
+describe('Blitzr', function () {
+    describe('#toStringQuery', function() {
+        it('should return string query', function () {
+            const data = {
+                string: '   a b c6890 -A- lorem    ',
+                bool: true,
+                boolean: false,
+                int: 10
+            };
+            const str = blitzr._toStringQuery(data);
+            assert.equal(str, '?string=a+b+c6890+-A-+lorem&bool=true&boolean=false&int=10');
+        });
+    });
+});
