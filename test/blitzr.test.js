7d4b74b47dd8b8808213eb20bd9eb41d680aca0a
yoann <yoann@blitzr.com>
2016-04-26 09:46:03 +0200

    add fail test

--- test/blitzr.test.js
+++ test/blitzr.test.js
@@ -13,5 +13,8 @@ describe('Blitzr', function () {
             const str = blitzr._toStringQuery(data);
             assert.equal(str, '?string=a+b+c6890+-A-+lorem&bool=true&boolean=false&int=10');
         });
+        it('faild', function() {
+            assert.equal(true, false)
+        })
     });
 });
