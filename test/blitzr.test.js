25417963943b14ed2c1d87a534d259165bf7a3e7
yoann <yoann@blitzr.com>
2016-04-26 16:32:48 +0200

    add search.artist() method

--- test/blitzr.test.js
+++ test/blitzr.test.js
@@ -1,17 +1,37 @@
 const assert = require('assert');
-const blitzr = require ('../blitzr.js');
+const Blitzr = require ('../blitzr.js');
+const blitzr = new Blitzr('f1a14162e95f6f0afd4d70e1ad526ad3');
 
 describe('Blitzr', function () {
-    describe('#toStringQuery', function() {
+    describe('#toQueryString', function() {
         it('should return string query', function () {
             const data = {
-                string: '   a b c6890 -A- lorem    ',
+                string: 'lorem ipsum',
+                filter: {},
+                zero: 0,
+                empty: '',
                 bool: true,
                 boolean: false,
-                int: 10
+                int: 10,
+                obj: {
+                    location: 'bordeaux',
+                    tag: 'jazz',
+                    num: 1337,
+                    bool: false
+                }
             };
-            const str = blitzr._toStringQuery(data);
-            assert.equal(str, '?string=a+b+c6890+-A-+lorem&bool=true&boolean=false&int=10');
+            const str = blitzr._toQueryString(data);
+            assert.equal(str, 'string=lorem%20ipsum&zero=0&bool=true&boolean=false&int=10&obj[location]=bordeaux&obj[tag]=jazz&obj[num]=1337&obj[bool]=false');
+        });
+    });
+    describe('#search.artist', function() {
+        it('should return data in an array', function() {
+            const data = {
+                query: 'the black keys'
+            }
+            const artist = blitzr.search.artist(data).then(res => {
+                assert.equal(typeof res, 'array')
+            })
         });
     });
 });
